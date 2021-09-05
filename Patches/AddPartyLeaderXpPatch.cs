f864def24906a34a0052759989e391166bf3c8cb
SNag-daphotons <soumadip@daphotons.in>
2021-09-05 18:37:30 -0400

    Initial

---
+++ Patches/AddPartyLeaderXpPatch.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+
+namespace PartyLeaderRoleXP.Patches
+{
+    [HarmonyPatch(typeof(Hero), "AddSkillXp")]
+    public class AddPartyLeaderXpPatch
+    {
+        private static FieldInfo hdFieldInfo = null;
+        static void Postfix(Hero __instance, SkillObject skill, float xpAmount)
+        {
+            try
+            {
+                if (xpAmount > 0  && __instance.PartyBelongedTo != null && !__instance.IsPartyLeader)
+                {
+                    MobileParty party = __instance.PartyBelongedTo;
+                    if (party.EffectiveScout == __instance && skill.Name.ToString() == "Scouting"
+                        || party.EffectiveEngineer == __instance && skill.Name.ToString() == "Engineering"
+                        || party.EffectiveSurgeon == __instance && skill.Name.ToString() == "Medicine"
+                        || party.EffectiveQuartermaster == __instance && skill.Name.ToString() == "Steward")
+                    {
+                        if (hdFieldInfo == null) GetFieldInfo();
+                        Hero partyLeader = party.LeaderHero ?? null;
+                        if (partyLeader != null)
+                        {
+                            HeroDeveloper plhd = (HeroDeveloper)hdFieldInfo.GetValue(partyLeader);
+                            float newXpAmount = (float)(xpAmount * 0.25); //(int)Math.Ceiling(xpAmount * 0.5);
+                            plhd.AddSkillXp(skill, newXpAmount, true, true);
+                            //InformationManager.DisplayMessage(new InformationMessage($"{skill.Name} Role xp: {xpAmount} Leader xp: {newXpAmount}", TaleWorlds.Library.Color.Black));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                InformationManager.DisplayMessage(new InformationMessage($"An exception occurred whilst trying to apply role xp to party leader: {ex}", TaleWorlds.Library.Color.Black));
+            }
+        }
+
+        private static void GetFieldInfo()
+        {
+            hdFieldInfo = typeof(Hero).GetField("_heroDeveloper", BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+    }
+}
---
+++ SubModule.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace PartyLeaderRoleXP
+{
+    public class SubModule : MBSubModuleBase
+    {
+        private static Harmony harmony = null;
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            if (harmony == null)
+            {
+                try
+                {
+                    harmony = new Harmony("mod.bannerlord.PartyLeaderRoleXP");
+                    harmony.PatchAll();
+                }
+                catch (Exception ex)
+                {
+                    InformationManager.DisplayMessage(new InformationMessage($"Error Initialising Role XP for Party Leader: {ex.Message}", TaleWorlds.Library.Color.Black));
+                }
+            }
+        }
+    }
+}
